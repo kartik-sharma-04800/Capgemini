4bfc9fafc9076497ce510ee77ef5917a8ba03fae
unknown <kartiks04800@gmail.com>
2026-01-08 10:14:48 +0530

    level1

---
+++ Java_String_Lvl1/src/StringOutOfBoundsExceptionDemo.java
@@ -0,0 +1,27 @@
+public class StringOutOfBoundsExceptionDemo {
+
+
+    public static void handleException(String text)
+    {
+        try{
+            text.charAt(text.length());
+        }
+        catch(StringIndexOutOfBoundsException e)
+        {
+            System.out.println("StringIndexOutOfBoundsException Handled Successfully"+" "+e.getMessage());
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        String text = "abc";
+
+
+        handleException(text);
+
+
+
+    }
+
+}
