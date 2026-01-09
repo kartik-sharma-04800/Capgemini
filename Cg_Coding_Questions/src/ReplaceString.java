72139d44dc5230605ee98aedb0526ffefee899eb
unknown <kartiks04800@gmail.com>
2026-01-09 10:37:27 +0530

    questions_done

---
+++ Cg_Coding_Questions/src/ReplaceString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReplaceString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        str = str.replace(" ","-");
+
+        System.out.println(str);
+    }
+
+}
